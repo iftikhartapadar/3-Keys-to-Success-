8b1c18b001a0e16d5098c3f37ccc682985bc6a62
Iftikhar Tapadar <38772501+iftikhartapadar@users.noreply.github.com>
2018-11-15 17:49:16 -0500

    First Commit

---
+++ 3 Keys to Success/3 Keys to Success/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  3 Keys to Success
+//
+//  Created by Code Next Student on 11/15/18.
+//  Copyright © 2018 Code Next Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 3 Keys to Success/3 Keys to SuccessTests/__Keys_to_SuccessTests.swift
@@ -0,0 +1,34 @@
+//
+//  __Keys_to_SuccessTests.swift
+//  3 Keys to SuccessTests
+//
+//  Created by Code Next Student on 11/15/18.
+//  Copyright © 2018 Code Next Student. All rights reserved.
+//
+
+import XCTest
+@testable import __Keys_to_Success
+
+class __Keys_to_SuccessTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
